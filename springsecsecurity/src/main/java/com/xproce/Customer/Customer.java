428ce2432b564fd31760a89caf6ef18ac5934146
Bouyaz <156103017+Bouyaz@users.noreply.github.com>
2024-01-09 16:01:19 +0100

    Add files via upload

---
+++ springsecsecurity/src/main/java/com/xproce/Customer/Customer.java
@@ -0,0 +1,20 @@
+package com.xproce.Customer;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Entity
+@NoArgsConstructor
+@AllArgsConstructor
+@Getter
+@Setter
+public class Customer {
+    @Id
+    int Id;
+    String Email,Pwd,Role;
+
+}
